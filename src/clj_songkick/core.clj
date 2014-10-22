e57d922ef90ce7f1029182e5ab0659d4bec9702b
Nick McDonnough <nickmcd@gmail.com>
2014-10-22 14:51:48 -0500

    Add some helpers for building urls

--- src/clj_songkick/core.clj
+++ src/clj_songkick/core.clj
@@ -12,3 +12,17 @@
 (def city-search-url "http://api.songkick.com/api/3.0/search/locations.json?apikey=") ;; "query="
 (def latlong-search-url "http://api.songkick.com/api/3.0/search/locations.json?apikey=") ;; "location=geo:{lat,lng}"
 
+;;;;;;
+;; Helpers
+;;;;;;
+(defn plusify [string] (string/join "+" (string/split string #"\s")))
+(defn drop-nils [& parameters] (into {} (filter second (apply hash-map parameters))))
+
+;; this is to reduce need for a dependency like cheshire
+(defn paramify
+  [param-map]
+  (let [values (map plusify (vals param-map))
+        params (map name (keys param-map))
+        pairs (interleave params values)]
+    (string/join "&" (map #(string/join "=" %) (partition 2 pairs)))))
+
